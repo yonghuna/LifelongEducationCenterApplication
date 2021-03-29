ae3f57d170ed194c31babd23dfdbdd8ddbaacd68
daeeung <aet401@naver.com>
2021-03-29 19:33:08 +0900

    login main view,memberregistraion -daeeung

--- app/src/main/java/com/example/lifelongeducationcenterapplication/Login.java
+++ app/src/main/java/com/example/lifelongeducationcenterapplication/Login.java
@@ -1,14 +1,26 @@
 package com.example.lifelongeducationcenterapplication;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Login extends AppCompatActivity {
 
+    Button btlogin,btresgister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        btlogin = (Button) findViewById(R.id.loginbutton);
+        btresgister = (Button)findViewById(R.id.registerbutton1);
     }
+
+    //로그인인
+   private void runLogin(){
+
+    }
+    //회원 가입
+    public void goResgister(){}
+
 }
