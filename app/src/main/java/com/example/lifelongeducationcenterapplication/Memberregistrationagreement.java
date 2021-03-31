993fe41a1f5a9086b3ce8b2d333ce7f23321d51b
daeeung <aet401@naver.com>
2021-04-01 02:57:06 +0900

    login main view,memberregistraion 로그인 기능 수정 -daeeung

--- app/src/main/java/com/example/lifelongeducationcenterapplication/Memberregistrationagreement.java
+++ app/src/main/java/com/example/lifelongeducationcenterapplication/Memberregistrationagreement.java
@@ -2,6 +2,7 @@ package com.example.lifelongeducationcenterapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,6 +21,22 @@ public class Memberregistrationagreement extends AppCompatActivity {
         radiobutton3 = (Button)findViewById(R.id.radioButton3); //약관 동의
         radiobutton4 = (Button)findViewById(R.id.radioButton4); //약관 취소
         setContentView(R.layout.activity_member_registration_agreement);
+
+        //약관 동의 버튼 기능
+        btagree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        //취소 버튼 ->로그인 화면으로 넘어감
+        btcancle.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(Memberregistrationagreement.this, Login.class);
+                startActivity(intent);
+            }
+        });
     }
     //동의 버튼 ->회원등록
     public void goSignUpActivity(){startActivity(new Intent(this,SignUpActivity.class));}
