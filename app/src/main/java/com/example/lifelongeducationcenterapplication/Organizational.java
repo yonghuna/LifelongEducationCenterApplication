625112e2736f274542563700bc15516191bb5f2b
yonghuna <kooaas@daum.net>
2021-04-12 16:39:54 +0900

    교육소개

---
+++ app/src/main/java/com/example/lifelongeducationcenterapplication/Organizational.java
@@ -0,0 +1,15 @@
+package com.example.lifelongeducationcenterapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Organizational extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.introduction_organizational);
+
+
+    }
+}
