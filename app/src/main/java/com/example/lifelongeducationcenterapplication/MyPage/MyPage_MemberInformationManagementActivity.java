be783e0b0d2899a8263f1db410c938d23bc045e3
daeeung <aet401@naver.com>
2021-04-12 17:44:52 +0900

    사이트맵 class xml UI commit  -daeeung

---
+++ app/src/main/java/com/example/lifelongeducationcenterapplication/MyPage/MyPage_MemberInformationManagementActivity.java
@@ -0,0 +1,14 @@
+package com.example.lifelongeducationcenterapplication.MyPage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.lifelongeducationcenterapplication.R;
+public class MyPage_MemberInformationManagementActivity extends AppCompatActivity {
+    //마이페이지 회원정보관리
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_page__member_information_management);
+    }
+}
