c2cb17449e61d64755c6b11087358451600288f2
yonghuna <kooaas@daum.net>
2021-04-22 22:09:18 +0900

    일반 과정 수정 , 회원가입 중복테스트

--- app/src/main/java/com/example/lifelongeducationcenterapplication/Generalcurriculum/LearnmoreaboutforeignlanguagecoursesActivity.java
+++ app/src/main/java/com/example/lifelongeducationcenterapplication/Generalcurriculum/LearnmoreaboutforeignlanguagecoursesActivity.java
@@ -29,7 +29,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 import static com.example.lifelongeducationcenterapplication.RemoteService.BASE_URL;
 
 public class LearnmoreaboutforeignlanguagecoursesActivity extends AppCompatActivity {
-    //외국어 과정 상세보기
+    //과정 상세보기
 
     Retrofit retrofit1; //httpclient library
     RemoteService rs1; //DB를 위한 인터페이스
@@ -61,6 +61,7 @@ public class LearnmoreaboutforeignlanguagecoursesActivity extends AppCompatActiv
         btRegister = (Button) findViewById(R.id.Courseregistrationbt1);
         listLecture = (ListView) findViewById(R.id.Lecturelist);
 
+        // number 값을 받아서 구분 해준다
         Intent intent = getIntent();
         number = intent.getIntExtra("number", 1); // pk로 구분
 
@@ -80,6 +81,7 @@ public class LearnmoreaboutforeignlanguagecoursesActivity extends AppCompatActiv
     @Override
     protected void onResume() {
 
+        // 다른 detail 부분 
         Call<List<LectureDetail>> call1 = rs1.lectureDetail(number);//detail
         call1.enqueue(new Callback<List<LectureDetail>>() {//enqueue 메소드 실행
             @Override
@@ -118,6 +120,7 @@ public class LearnmoreaboutforeignlanguagecoursesActivity extends AppCompatActiv
             }
         });
 
+        // week 주 내용 과정 불러오기
         Call<List<LectureWeek>> call2 = rs2.lectureWeek(number);//week
         call2.enqueue(new Callback<List<LectureWeek>>() {
             @Override
@@ -139,11 +142,8 @@ public class LearnmoreaboutforeignlanguagecoursesActivity extends AppCompatActiv
         super.onResume();
     }
 
-
-
-
-
-
+    
+// week content convertview 에 올리기
     class MyAdapter extends BaseAdapter {
         @Override
         public int getCount() {
--- app/src/main/java/com/example/lifelongeducationcenterapplication/RemoteService.java
+++ app/src/main/java/com/example/lifelongeducationcenterapplication/RemoteService.java
@@ -13,25 +13,25 @@ import retrofit2.http.Query;
 public interface RemoteService {
     public static final String BASE_URL = "http://" + Ipaddress.ip  +"/LifelongEducationCenterApplication-back/Android/";
 
-
+    // 공지사항
     @GET("notice.jsp")
     @Headers("Content-Type:application/x-www-form-urlencoded; charset=utf-8")
     Call<List<Notice>> notice();//수강목록
 
-
+    // 강의
     @GET("lecture.jsp")
     @Headers("Content-Type:application/x-www-form-urlencoded; charset=utf-8")
     Call<List<Lecture>> lecture(
             @Query("division") String division);//수강목록
 
-
+    // 강의 세부 내용 
     @GET("lectureDetail.jsp")
     @Headers("Content-Type:application/x-www-form-urlencoded; charset=utf-8")
     Call<List<LectureDetail>> lectureDetail(
             @Query("number") int number);//수강 상세보기
 
 
-
+    // 강의 주 내용
     @GET("lectureWeek.jsp")
     @Headers("Content-Type:application/x-www-form-urlencoded; charset=utf-8")
     Call<List<LectureWeek>> lectureWeek(
@@ -39,6 +39,7 @@ public interface RemoteService {
 
 
 
+    //로그인
     @FormUrlEncoded
     @POST("login.jsp")
     @Headers("Content-Type:application/x-www-form-urlencoded; charset=utf-8")
@@ -47,6 +48,18 @@ public interface RemoteService {
             @Field("phoneNumber") String phoneNumber,
             @Field("password") String password); //로그인
 
+    
+    //로그인 체크 
+    @FormUrlEncoded
+    @POST("loginCheck.jsp")
+    @Headers("Content-Type:application/x-www-form-urlencoded; charset=utf-8")
+    Call<CommunicationResult> loginCheck(
+            @Field("name") String name,
+            @Field("birth") String birth,
+            @Field("sex") String sex); //로그인
+
+
+    // 회원가입
     @FormUrlEncoded
     @POST("register.jsp")
     @Headers("Content-Type:application/x-www-form-urlencoded; charset=utf-8")
