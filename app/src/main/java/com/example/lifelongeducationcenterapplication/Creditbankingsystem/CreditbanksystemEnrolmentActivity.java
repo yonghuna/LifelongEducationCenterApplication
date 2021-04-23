9213bc46928167107456f75823966d1d9153b368
yonghuna <kooaas@daum.net>
2021-04-24 01:27:52 +0900

    메인 공지사항 표시

--- app/src/main/java/com/example/lifelongeducationcenterapplication/Creditbankingsystem/CreditbanksystemEnrolmentActivity.java
+++ app/src/main/java/com/example/lifelongeducationcenterapplication/Creditbankingsystem/CreditbanksystemEnrolmentActivity.java
@@ -1,15 +1,53 @@
 package com.example.lifelongeducationcenterapplication.Creditbankingsystem;
+import com.example.lifelongeducationcenterapplication.Account.Login;
+import com.example.lifelongeducationcenterapplication.Generalcurriculum.ForeignlanguagecourseActivity;
+import com.example.lifelongeducationcenterapplication.Generalcurriculum.LearnmoreaboutforeignlanguagecoursesActivity;
+import com.example.lifelongeducationcenterapplication.Lecture;
 import com.example.lifelongeducationcenterapplication.R;
+import com.example.lifelongeducationcenterapplication.RemoteService;
+import com.example.lifelongeducationcenterapplication.StaticId;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import static com.example.lifelongeducationcenterapplication.RemoteService.BASE_URL;
 
 public class CreditbanksystemEnrolmentActivity extends AppCompatActivity {
     //학점은행제 수강신청 액티비티
+
+    Button btKorean, btAthletic, btOperation;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().setTitle("수강신청");
         setContentView(R.layout.activity_creditbanksystem_enrolment);
+
+
+        btKorean = (Button) findViewById(R.id.bt_tab1);
+        btAthletic =  (Button) findViewById(R.id.bt_tab2);
+        btOperation =  (Button) findViewById(R.id.bt_tab3);
+
+
     }
+
 }
