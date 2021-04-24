7560da1fef8c82768aba734b8b990711f9336460
daeeung <aet401@naver.com>
2021-04-25 08:22:55 +0900

    학점은행제 전공안내 UI등록

--- app/src/main/java/com/example/lifelongeducationcenterapplication/Creditbankingsystem/CreditbanksystemMajorInformation3Activity.java
+++ app/src/main/java/com/example/lifelongeducationcenterapplication/Creditbankingsystem/CreditbanksystemMajorInformation3Activity.java
@@ -4,12 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import com.example.lifelongeducationcenterapplication.R;
-public class CreditbanksystemMajorInformationActivity extends AppCompatActivity {
-    //학점은행제 전공안내 외국어로서의 한국어학
+public class CreditbanksystemMajorInformation3Activity extends AppCompatActivity {
+    //학점은행제 전공안내  경제학
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().setTitle("전공안내");
-        setContentView(R.layout.activity_creditbanksystem_major_information);
+        setContentView(R.layout.activity_creditbanksystem_major_information3);
     }
 }
