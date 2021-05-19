bd46a2a871cdbcd3f3a69238c947ccd30730a04d
daeeung <aet401@naver.com>
2021-05-19 19:27:57 +0900

    갤러리 수정

---
+++ app/src/main/java/com/example/lifelongeducationcenterapplication/Community/community_gallerydetails.java
@@ -0,0 +1,16 @@
+package com.example.lifelongeducationcenterapplication.Community;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.lifelongeducationcenterapplication.R;
+
+public class community_gallerydetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_community_gallerydetails);
+    }
+}
